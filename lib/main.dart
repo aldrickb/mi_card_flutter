8e7a1415b52e02a6b9968cfef2aad37b096dd2e1
Aldrick Bonaobra <aldrickbonaobra@gmail.com>
2020-06-13 13:02:34 +0800

    Add inkwell animation

--- lib/main.dart
+++ lib/main.dart
@@ -48,35 +48,45 @@ class MyApp extends StatelessWidget {
               ),
               Card(
                 margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: ListTile(
-                  leading: Icon(
-                    Icons.email,
-                    color: Colors.lightBlue
-                  ),
-                  title: Text(
-                    'hello@aldrickb.com',
-                    style: TextStyle(
-                      fontSize: 16.0,
-                      color: Colors.lightBlue.shade900,
-                      fontFamily: 'SourceSansPro-Regular'
-                    )
+                child: InkWell(
+                  onTap: () {
+
+                  },
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.lightBlue
+                    ),
+                    title: Text(
+                      'hello@aldrickb.com',
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        color: Colors.lightBlue.shade900,
+                        fontFamily: 'SourceSansPro-Regular'
+                      )
+                    ),
                   ),
                 )
               ),
               Card(
                 margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: ListTile(
-                  leading: Icon(
-                    Icons.public,
-                    color: Colors.lightBlue
-                  ),
-                  title: Text(
-                    'aldrickb.com',
-                    style: TextStyle(
-                      fontSize: 16.0,
-                      color: Colors.lightBlue.shade900,
-                      fontFamily: 'SourceSansPro-Regular'
-                    )
+                child: InkWell(
+                  onTap: () {
+                    
+                  },
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.public,
+                      color: Colors.lightBlue
+                    ),
+                    title: Text(
+                      'aldrickb.com',
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        color: Colors.lightBlue.shade900,
+                        fontFamily: 'SourceSansPro-Regular'
+                      )
+                    ),
                   ),
                 )
               )
