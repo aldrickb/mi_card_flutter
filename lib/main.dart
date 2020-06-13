9c4635d2ff8a13f63b9f9e8e76bb9914f0284646
Aldrick Bonaobra <aldrickbonaobra@gmail.com>
2020-06-13 12:16:25 +0800

    Add phone and email card

--- lib/main.dart
+++ lib/main.dart
@@ -37,7 +37,55 @@ class MyApp extends StatelessWidget {
                   letterSpacing: 2.5,
                   fontWeight: FontWeight.bold
                 )
-              )
+              ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                padding: EdgeInsets.all(10.0),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.phone,
+                      color: Colors.teal
+                    ),
+                    SizedBox(
+                      width: 10.0
+                    ),
+                    Text(
+                      '+63 939 512 7793',
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        color: Colors.teal.shade900,
+                        fontFamily: 'SourceSansPro-Regular'
+                      )
+                    )
+                  ],
+                )
+              ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                padding: EdgeInsets.all(10.0),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.email,
+                      color: Colors.teal
+                    ),
+                    SizedBox(
+                      width: 10.0
+                    ),
+                    Text(
+                      'hello@aldrickb.com',
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        color: Colors.teal.shade900,
+                        fontFamily: 'SourceSansPro-Regular'
+                      )
+                    )
+                  ],
+                )
+              ),
             ],
           )
         ),
